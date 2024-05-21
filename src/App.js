9d14272ff4881db8446ea25b101f56a43f9fe5c2
nourgm10 <nourgm10@gmail.com>
2024-05-21 10:37:21 +0200

    added Bootstrap

--- src/App.js
+++ src/App.js
@@ -5,25 +5,27 @@ import "./App.css";
 export default function App() {
   return (
     <div className="App">
-      <h1>Hello World</h1>
-      <footer>
-        This project was coded by{" "}
-        <a
-          href="https://www.linkedin.com/in/nourlivia/"
-          target="_blank"
-          rel="noreferrer"
-        >
-          Nour Ghallale
-        </a>{" "}
-        and is open-sourced on{" "}
-        <a
-          href="https://github.com/nourgm10/nours-react-weather-app"
-          target="_blank"
-          rel="noreferrer"
-        >
-          GitHub
-        </a>
-      </footer>
+      <div className="container">
+        <h1>Hello World</h1>
+        <footer>
+          This project was coded by{" "}
+          <a
+            href="https://www.linkedin.com/in/nourlivia/"
+            target="_blank"
+            rel="noreferrer"
+          >
+            Nour Ghallale
+          </a>{" "}
+          and is open-sourced on{" "}
+          <a
+            href="https://github.com/nourgm10/nours-react-weather-app"
+            target="_blank"
+            rel="noreferrer"
+          >
+            GitHub
+          </a>
+        </footer>
+      </div>
     </div>
   );
 }
