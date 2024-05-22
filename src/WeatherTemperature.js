9b30592ca6938f9ecdc19530df6b874491866e40
nourgm10 <nourgm10@gmail.com>
2024-05-22 14:50:58 +0200

    added useEffect and removed Farenheit option

--- src/WeatherTemperature.js
+++ src/WeatherTemperature.js
@@ -1,41 +1,10 @@
 import React, { useState } from "react";
 
 export default function WeatherTemperature(props) {
-  const [unit, setUnit] = useState("celsius");
-  function showFarenheit(event) {
-    event.preventDefault();
-    setUnit("farenheit");
-  }
-
-  function showCelsius(event) {
-    event.preventDefault();
-    setUnit("celsius");
-  }
-
-  if (unit === "celsius") {
-    return (
-      <span>
-        <span className="temperature">{Math.round(props.celsius)}</span>
-        <span className="unit">
-          °C{" "}
-          <a href="/" onClick={showFarenheit}>
-            | °F
-          </a>
-        </span>
-      </span>
-    );
-  } else {
-    let farenheit = (props.celsius * 9) / 5 + 32;
-    return (
-      <span>
-        <span className="temperature">{Math.round(farenheit)}</span>
-        <span className="unit">
-          °F{" "}
-          <a href="/" onClick={showCelsius}>
-            | °C
-          </a>
-        </span>
-      </span>
-    );
-  }
+  return (
+    <span>
+      <span className="temperature">{Math.round(props.celsius)}</span>
+      <span className="unit">°C</span>
+    </span>
+  );
 }
